578f3ee23ef1a4afeed7b78eb5f2c6a582113dc0
ghazallsyria-cpu <ghazallsyria@gmail.com>
2026-03-30 11:05:21 +0300

    feat: Import Question type
    Expose the Question type from the types/question file for use in other parts of the application. This allows for better type safety when dealing with question-related data.

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 import * as validations from '../lib/validations';
+import { Question } from './question';
 
 export * from './question';
 export * from '../lib/validations';
