0843376688585296db448fb8ac7991a19366457b
ghazallsyria-cpu <ghazallsyria@gmail.com>
2026-04-15 02:43:31 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,34 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'standalone',
 
-  // 🚀 تغيير المعرف مع كل عملية بناء لإجبار جلب ملفات Next.js الجديدة
   generateBuildId: async () => {
     return `build-${new Date().getTime()}`;
   },
 
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'res.cloudinary.com', port: '', pathname: '/**' },
-      { protocol: 'https', hostname: 'picsum.photos', port: '', pathname: '/**' },
+      { protocol: 'https', hostname: 'res.cloudinary.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'picsum.photos', pathname: '/**' },
     ],
-  },
-
-  async headers() {
-    return [
-      {
-        source: '/manifest.json',
-        headers: [
-          { key: 'Content-Type', value: 'application/manifest+json; charset=utf-8' },
-        ],
-      },
-      // 🚀 إجبار المتصفح على تحميل "ملف الانتحار" وعدم حفظه أبداً في الكاش!
-      {
-        source: '/sw.js',
-        headers: [
-          { key: 'Cache-Control', value: 'no-cache, no-store, must-revalidate, max-age=0' },
-        ],
-      }
-    ];
   },
 };
 
